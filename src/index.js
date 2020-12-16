98c5daa00b3b6e8c2dfb2b0099fe3f32dbb80bb9
ghislaine34 <diot.ghislaine@gmail.com>
2020-12-16 10:03:04 +0100

    personnaliser pages 404 et 500

--- src/index.js
+++ src/index.js
@@ -18,6 +18,18 @@ app.get('/maintenance', (req, res) => res.render('maintenance'));
 app.get('/contact', (req, res) => res.render('contact'));
 app.get('/reservations', (req, res) => res.render('reservations'));
 
+//Appel de la page 404
+app.use(function (request, response) {
+    response.status(404).sendFile(__dirname + '/resources/static/assets/images/404.jpg')
+});
+
+//Personnalise erreur 500
+app.use(function(err, request, response, next) {
+    console.log(err);
+    response.status(500).sendFile(__dirname + 'resources/static/assets/500.jpg')
+});
+
+
 app.get('/mentionLegales', (req, res) => res.render('mentionLegales'));
 
 app.listen(port);
